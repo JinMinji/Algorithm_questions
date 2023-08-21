09fd868e2b3260d1902fa79f2f6850a0b698d03c
Mingzhi <alswl_1110@naver.com>
2023-08-21 23:30:00 +0900

    디렉토리 변경

---
+++ Python/LeetCode/Contest358/Contest358_2815.py
@@ -0,0 +1,14 @@
+# Contest358, Q1
+
+from typing import List
+
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+
+
+
+if __name__ == "__main__":
+    test = Solution()
+    print(test.maxSum(nums = [51,71,17,24,42]))
+    print(test.maxSum(nums = [1,2,3,4]))
+    # print(test.maxSum(nums = [2,1,3]))
---
+++ Python/LeetCode/Contest358/Contest358_2816.py
@@ -0,0 +1,18 @@
+# Contest358, Q2
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+
+if __name__ == "__main__":
+    test = Solution()
+    print(test.doubleIt(head = [1,8,9]))
+    print(test.doubleIt(head = [9,9,9]))
+    # print(test.doubleIt(head = [9,9,9]))
